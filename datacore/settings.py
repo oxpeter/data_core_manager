903f1b4d0fcd2ad400efa32bdca8afcf637cc624
oxpeter <oxpeter+git@gmail.com>
2017-09-27 21:16:26 -0400

    fix passing of env variables to setting.py

--- datacore/settings.py
+++ datacore/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a*^b1b7s$4ym*@q-kuph7u)v)oozx_9o%%17y8$a8h%$9gy4^*'
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
